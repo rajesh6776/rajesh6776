c5c932d6e9e868bf47441d3bf6180cffbc54fcda
rajesh6776 <104759576+rajesh6776@users.noreply.github.com>
2022-08-26 06:14:11 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! For - Auto Updat";
+    private final String message = "Hello World! For - Auto Update";
 
     public App() {}
 
